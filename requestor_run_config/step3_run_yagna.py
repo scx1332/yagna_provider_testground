b8b5c74c984a879e93a60a43334ee4fc8b977f7d
scx1332 <sieciech.czajka@golem.network>
2022-02-28 11:16:20 +0100

    Copy yagna

--- requestor_run_config/step3_run_yagna.py
+++ requestor_run_config/step3_run_yagna.py
@@ -1,7 +1,20 @@
 import os
+import shutil
 from subprocess import Popen
 
 yagna_exe_path = r"yagna.exe"
+
+source_yagna_directory = r"C:\golem\yagna\target\debug"
+target_yagna_directory = r"."
+
+def copy_file_local(srcDir, targetDir):
+    if os.path.isfile(srcDir):
+        print("Copying and overwriting file: {} => {}".format(srcDir, targetDir))
+        shutil.copy2(srcDir, targetDir)
+
+
+copy_file_local(os.path.join(source_yagna_directory, "yagna.exe"), target_yagna_directory)
+
 command = f"{yagna_exe_path} service run"
 print(command)
 p1 = Popen(command, shell=True)

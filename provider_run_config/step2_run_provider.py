39a34b7ff166978cd42754197b3400abed4b0522
scx1332 <sieciech.czajka@golem.network>
2022-03-02 17:03:10 +0100

    wip

--- provider_run_config/step2_run_provider.py
+++ provider_run_config/step2_run_provider.py
@@ -1,10 +1,23 @@
 import os
 from subprocess import Popen
 import shutil
+import platform
+import json
 
-yagna_exe_path = r"yagna.exe"
+def open_config():
+    if platform.system() == "Linux":
+        config_path="config_linux.json"
+    else:
+        config_path="config.json"
 
-source_runtime_directory = r"C:\golem\ya-runtime-vm\runtime\init-container"
+    with open(config_path, "r") as f:
+        return json.loads(f.read())
+
+config_params = open_config()
+
+yagna_exe_path = config_params["yagna_executable"]
+
+ya_runtime_vm_directory = config_params["step2"]["ya_runtime_vm_directory"]
 target_runtime_directory = r"plugins\ya-runtime-vm\runtime"
 
 source_runtime_exe_directory = r"C:\golem\ya-runtime-vm\target\debug"
@@ -18,25 +31,28 @@ def copy_file_local(srcDir, targetDir):
         print("Copying and overwriting file: {} => {}".format(srcDir, targetDir))
         shutil.copy2(srcDir, targetDir)
 
+qemu_executable = config_params["step2"]["qemu_executable"]
 
-copy_file_local(os.path.join(source_runtime_directory, "bios-256k.bin"), target_runtime_directory)
-copy_file_local(os.path.join(source_runtime_directory, "init"), target_runtime_directory)
-copy_file_local(os.path.join(source_runtime_directory, "initramfs.cpio.gz"), target_runtime_directory)
-copy_file_local(os.path.join(source_runtime_directory, "kvmvapic.bin"), target_runtime_directory)
-copy_file_local(os.path.join(source_runtime_directory, "linuxboot_dma.bin"), target_runtime_directory)
-copy_file_local(os.path.join(source_runtime_directory, "self-test.gvmi"), target_runtime_directory)
-copy_file_local(os.path.join(source_runtime_directory, "ubuntu.gvmi"), target_runtime_directory)
-copy_file_local(os.path.join(source_runtime_directory, "qemu-system-x86_64.exe"), target_runtime_directory)
-copy_file_local(os.path.join(source_runtime_directory, "vmlinuz-virt"), target_runtime_directory)
+copy_file_local(os.path.join(ya_runtime_vm_directory, "runtime", "poc", "runtime", "bios-256k.bin"), target_runtime_directory)
+copy_file_local(os.path.join(ya_runtime_vm_directory, "runtime", "poc", "runtime", "kvmvapic.bin"), target_runtime_directory)
+copy_file_local(os.path.join(ya_runtime_vm_directory, "runtime", "poc", "runtime", "linuxboot_dma.bin"), target_runtime_directory)
+copy_file_local(os.path.join(ya_runtime_vm_directory, "runtime", "poc", "runtime", qemu_executable), target_runtime_directory)
+copy_file_local(os.path.join(ya_runtime_vm_directory, "runtime", "image", "self-test.gvmi"), target_runtime_directory)
+copy_file_local(os.path.join(ya_runtime_vm_directory, "runtime", "init-container", "initramfs.cpio.gz"), target_runtime_directory)
+copy_file_local(os.path.join(ya_runtime_vm_directory, "runtime", "init-container", "vmlinuz-virt"), target_runtime_directory)
 
-copy_file_local(os.path.join(source_runtime_exe_directory, "ya-runtime-vm.exe"), target_runtime_exe_directory)
+copy_file_local(os.path.join(ya_runtime_vm_directory, "target", "debug", "ya-runtime-vm.exe"), target_runtime_exe_directory)
 
-copy_file_local(os.path.join(source_fileserver_directory, "ya-vm-file-server.exe"), target_fileserver_directory)
+if platform.system() == "Windows":
+    copy_file_local(os.path.join(source_fileserver_directory, "ya-vm-file-server.exe"), target_fileserver_directory)
 
 yaprovider_exe_path = r"ya-provider.exe"
 
-payment_init = Popen(f"{yagna_exe_path} payment init --receiver --network rinkeby --account 0xc596aee002ebe98345ce3f967631aaf79cfbdf41", shell=True)
 
-with Popen(f"{yaprovider_exe_path} run", shell=True) as p1:
-    pass
+
+#payment_init = Popen(f"{yagna_exe_path} payment init --receiver --network rinkeby --account 0xc596aee002ebe98345ce3f967631aaf79cfbdf41", shell=True)
+
+
+#with Popen(f"{yaprovider_exe_path} run", shell=True) as p1:
+#    pass
 

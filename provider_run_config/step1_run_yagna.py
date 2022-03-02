cddb0012efc0f806d280c42cce79ecd3bb78e30b
scx1332 <sieciech.czajka@golem.network>
2022-03-02 15:28:50 +0100

    Added copy to first python file

--- provider_run_config/step1_run_yagna.py
+++ provider_run_config/step1_run_yagna.py
@@ -1,5 +1,18 @@
 import os
+import shutil
 from subprocess import Popen
+
+
+source_yagna_directory = r"C:\golem\yagna\target\debug"
+target_yagna_directory = r"."
+
+def copy_file_local(srcDir, targetDir):
+    if os.path.isfile(srcDir):
+        print("Copying and overwriting file: {} => {}".format(srcDir, targetDir))
+        shutil.copy2(srcDir, targetDir)
+
+
+copy_file_local(os.path.join(source_yagna_directory, "yagna.exe"), target_yagna_directory)
 
 yagna_exe_path = r"yagna.exe"
 command = f"{yagna_exe_path} service run"

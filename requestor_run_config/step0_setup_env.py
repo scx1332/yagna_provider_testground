3968b18b4dd3eb24d54c9fcb2a945e3287e28018
scx1332 <sieciech.czajka@golem.network>
2022-03-03 17:12:31 +0100

    Added step0

---
+++ requestor_run_config/step0_setup_env.py
@@ -0,0 +1,26 @@
+import os
+import random
+import string
+
+# run this script once to setup environment variables for provider
+
+
+randname = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+
+SUBNET_NAME = "localtest"
+NODE_NAME = f"req_node_{randname}"
+
+with open(".env.template", "r") as f:
+    template = f.read()
+    template = template.replace("%%SUBNET%%", SUBNET_NAME)
+    template = template.replace("%%NODE_NAME%%", NODE_NAME)
+    with open(".env", "w") as f2:
+        f2.write(template)
+
+
+
+
+
+
+
+

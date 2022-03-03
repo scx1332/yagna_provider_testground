569561c0afd7191e03cd15b34758e672f6e9c823
scx1332 <sieciech.czajka@golem.network>
2022-03-03 18:26:25 +0100

    wip

--- common/common.py
+++ common/common.py
@@ -2,6 +2,8 @@ import os
 import shutil
 import platform
 import json
+import subprocess
+
 
 def open_config():
     if platform.system() == "Linux":
@@ -11,6 +13,7 @@ def open_config():
 
     with open(config_path, "r") as f:
         return json.loads(f.read())
+
 
 def copy_file_local(srcDir, targetDir):
     # if os.path.isfile(srcDir):
@@ -22,3 +25,74 @@ def copy_file_local(srcDir, targetDir):
     except Exception as e:
         print(f"ERROR: while copying file {srcDir}, reason {e}")
         raise Exception(f"ERROR: while copying file {srcDir}, reason {e}")
+
+
+def _create_yagna_appkey(yagna_exe):
+    command = f"{yagna_exe} app-key create auto-provider-app-key"
+    print(f"Executing command {command}")
+
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    print(out)
+    pass
+
+
+def _extract_yagna_appkey(yagna_exe):
+    command = f"{yagna_exe} app-key list --json"
+    print(f"Executing command {command}")
+
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    yagna_response = out.decode('utf-8').strip()
+    yagna_error = err.decode('utf-8').strip()
+
+    print(f"yagna_response: {yagna_response}")
+
+    if yagna_error:
+        print(f"yagna_error: {yagna_error}")
+
+    if "Called service `/local/appkey/List` is unavailable" in yagna_error:
+        raise Exception("Probably cannot connect to yagna service. Check if yagna service is running.")
+
+    obj = json.loads(yagna_response)
+
+    key_idx = obj["headers"].index("key")
+
+    if len(obj["values"]) == 0:
+        raise Exception("NO KEYS FOUND")
+
+    if len(obj["values"]) > 1:
+        print("MULTIPLE KEYS FOUND, RETURNING FIRST")
+
+    yagna_appkey = obj["values"][0][key_idx]
+    print(f"Your yagna appkey: {yagna_appkey}")
+    return yagna_appkey
+
+
+def get_or_create_yagna_key(yagna_exe):
+    tries = 0
+    while True:
+        create_app_key = False
+        if tries > 3:
+            raise Exception("Failed to obtain yagna key")
+        try:
+            yagna_app_key = _extract_yagna_appkey(yagna_exe)
+            os.environ["YAGNA_APPKEY"] = yagna_app_key
+            break
+        except Exception as ex:
+            if str(ex) == "NO KEYS FOUND":
+                create_app_key = True
+            else:
+                raise ex
+
+        if create_app_key:
+            _create_yagna_appkey(yagna_exe)
+            print("Yagna app-key created")
+
+        tries += 1
+
+
+
+
+
+
--- yapapi-volumes-benchmark/volume_benchmark.py
+++ yapapi-volumes-benchmark/volume_benchmark.py
@@ -6,23 +6,30 @@ from yapapi import Golem, Task, WorkContext
 from yapapi.log import enable_default_logger
 from yapapi.payload import vm
 import pathlib
+import subprocess
+import json
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from common.common import open_config, get_or_create_yagna_key
+
+config = open_config()
+
+SUBNET_NAME = config["subnet"]
+
 
 async def worker(ctx: WorkContext, tasks: AsyncIterable[Task]):
     script_dir = pathlib.Path(__file__).resolve().parent
-    scene_path = str(script_dir / "vol_test.py")
+    scene_path = str(script_dir / "vol_test_sent_to_provider.py")
 
     print(scene_path)
 
     async for task in tasks:
         script = ctx.new_script()
-        script.upload_file(scene_path, "/golem/input/script.py")
+        script.upload_file(scene_path, "/golem/input/vol_test.py")
         folder = "/golem/output"
-        bigfilesize = 1000000000
+        bigfilesize = 10000
         repetitions = 10
-
-
-
-        future_result = script.run("/usr/local/bin/python", "/golem/input/script.py", "--folder", folder, "--bigfilesize", str(bigfilesize), "--repetitions", str(repetitions))
+        future_result = script.run("/usr/local/bin/python", "/golem/input/vol_test.py", "--folder", folder, "--bigfilesize", str(bigfilesize), "--repetitions", str(repetitions))
 
         yield script
 
@@ -38,8 +45,7 @@ async def main():
     tasks = [Task(data=None)]
 
     async with Golem(budget=1.0,
-                     #subnet_tag="scx_vm_subnet",
-                     subnet_tag="testnet",
+                     subnet_tag=SUBNET_NAME,
                      payment_network="rinkeby") as golem:
         async for completed in golem.execute_tasks(worker, tasks, payload=package):
             print(completed.result.stdout)
@@ -47,6 +53,16 @@ async def main():
 
 if __name__ == "__main__":
     enable_default_logger(log_file="hello.log")
+
+    yagna_exe = "yagna"
+
+    get_or_create_yagna_key(yagna_exe)
+
+    payment_init_command = f"{yagna_exe} payment init --sender --network rinkeby"
+    print(f"Running command: {payment_init_command}")
+    payment_init = subprocess.Popen(payment_init_command, shell=True)
+    payment_init.communicate()
+    print(f"Initialization finished. Running main loop...")
 
     loop = asyncio.new_event_loop()
     task = loop.create_task(main())

41d1be91b9755225cb35f3e75c4066d5dd87228c
scx1332 <sieciech@gmail.com>
2022-07-11 19:39:04 +0200

    Fixed script

--- runtime_requestor/step0_setup_env.py
+++ runtime_requestor/step0_setup_env.py
@@ -25,18 +25,31 @@ else:
     if config["use_own_central_net"]:
         CENTRAL_NET_HOST = config["central_net_addr"]
     else:
-        CENTRAL_NET_HOST = config["3.249.139.167:7464"]
+        CENTRAL_NET_HOST = "3.249.139.167:7464"
 
 
-with open(".env.template", "r") as f:
-    template = f.read()
-    template = template.replace("%%SUBNET%%", SUBNET_NAME)
-    template = template.replace("%%NODE_NAME%%", NODE_NAME)
-    template = template.replace("%%GSB_PORT%%", str(GSB_PORT))
-    template = template.replace("%%API_PORT%%", str(API_PORT))
-    template = template.replace("%%CENTRAL_NET_HOST%%", CENTRAL_NET_HOST)
-    with open(".env", "w") as f2:
-        f2.write(template)
+with open(".env", "w") as f2:
+    if config["use_own_central_net"]:
+        f2.write("CENTRAL_NET_HOST=" + config["central_net_addr"] + "\n")
+    f2.write("YAGNA_DATADIR=yagna_dir\n")
+    f2.write(f"SUBNET={SUBNET_NAME}\n")
+    f2.write(f"NODE_NAME={NODE_NAME}\n")
+    f2.write(f"GSB_URL=tcp://127.0.0.1:{GSB_PORT}\n")
+    f2.write(f"YAGNA_API_URL=http://127.0.0.1:{API_PORT}\n")
+    f2.write(f"YAGNA_MARKET_URL=http://127.0.0.1:{API_PORT}/market-api/v1/\n")
+    f2.write(f"YAGNA_ACTIVITY_URL=http://127.0.0.1:{API_PORT}/activity-api/v1/\n")
+    f2.write(f"YAGNA_PAYMENT_URL=http://127.0.0.1:{API_PORT}/payment-api/v1/\n")
+
+if 0:
+    with open(".env.template", "r") as f:
+        template = f.read()
+        template = template.replace("%%SUBNET%%", SUBNET_NAME)
+        template = template.replace("%%NODE_NAME%%", NODE_NAME)
+        template = template.replace("%%GSB_PORT%%", str(GSB_PORT))
+        template = template.replace("%%API_PORT%%", str(API_PORT))
+        template = template.replace("%%CENTRAL_NET_HOST%%", CENTRAL_NET_HOST)
+        with open(".env", "w") as f2:
+            f2.write(template)
 
 
 
